31316cc421aeea540aa4fa6e94ad7118f2ad2df5
Chunilal Banerjee <63794529+cbanerjee@users.noreply.github.com>
2021-07-26 14:45:00 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -9,8 +9,8 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = "${params.AWS_ACCESS_KEY_ID}"
-  secret_key =  "${params.AWS_SECRET_ACCESS_KEY}"
+  #access_key = "${params.AWS_ACCESS_KEY_ID}"
+  #secret_key =  "${params.AWS_SECRET_ACCESS_KEY}"
 }
 
 # Step1: Create a VPC
